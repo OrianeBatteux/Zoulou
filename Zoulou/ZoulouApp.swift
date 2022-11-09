ba51748c5374fe97d9e4ac2559f3c6092b950683
Oriane <oriane08@icloud.com>
2022-11-09 13:00:55 +0100

    Initial Commit

---
+++ Zoulou/ZoulouApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoulouApp.swift
+//  Zoulou
+//
+//  Created by Oriane Batteux on 09/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ZoulouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
